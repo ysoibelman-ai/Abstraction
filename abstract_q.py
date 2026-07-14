803564e63a2e8ecf6e79af58ad0c44e1dfe63094
yaakov-ai <ysoibelman@gmail.com>
2026-07-14 11:49:41 +0300

    question 2

--- abstract_q.py
+++ abstract_q.py
@@ -1,19 +1,42 @@
 from abc import ABC, abstractmethod
 
-# question 1
+# # question 1
+# class DeliveryMethod(ABC):
+
+#     @abstractmethod
+#     def deliver(self,order_id):
+#         pass
+
+# class BikeDelivery (DeliveryMethod):
+     
+#      def deliver(self,order_id):
+#         print (f"{order_id} Delivered by bike")
+
+# bike =BikeDelivery()
+# bike.deliver(101)
+
+# question 2
 class DeliveryMethod(ABC):
 
     @abstractmethod
     def deliver(self,order_id):
         pass
 
-class BikeDelivery (DeliveryMethod):
+class DroneDelivery (DeliveryMethod):
+     
      def deliver(self,order_id):
-        print (f"{order_id} Delivered by bike")
-bike =BikeDelivery()
-bike.deliver(101)
+        print (f"{order_id} dropped by dron at your door")
 
+        
+class CarDelivery (DeliveryMethod):
+     
+     def deliver(self,order_id):
+        print (f"{order_id} brought to your building by car")
 
+car = CarDelivery()
+car.deliver(202)
+drone = DroneDelivery()
+drone.deliver(202)
 
 
 
